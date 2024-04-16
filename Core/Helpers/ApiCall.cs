1492445284996a0bee8d8cb8ca633917bc22e7e7
YukineQ <mayberainq@gmail.com>
2024-04-16 14:53:40 +0300

    first commit

---
+++ Core/Helpers/ApiCall.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace Core.Helpers
+{
+    public sealed class ApiCall
+    {
+        public const string GET = "GET";
+        public const string POST = "POST";
+        public static HttpClient HttpClient { get; } = new HttpClient();
+
+        public ValueTask<T> GetAsync<T>(string url, IEnumerable<(string, string)> parameters = null)
+        {
+            return RequestAsync<T>(url, GET, parameters);
+        }
+
+        private async ValueTask<T> RequestAsync<T>(
+            string url, 
+            string method, 
+            IEnumerable<(string, string)> parameters = null
+        ) 
+        {
+            var post = string.Equals(method, POST, StringComparison.Ordinal);
+            var parray = parameters.ToArray();
+            var parameterString = parray.Select(p => $"{p.Item1}={p.Item2}");
+
+            var request = new HttpRequestMessage();
+            request.Headers.Add("Accept", "application/json");
+
+            if(post)
+            {
+                request.Method = HttpMethod.Post;
+                request.Content = new StreamContent(new MemoryStream(Encoding.UTF8.GetBytes(string.Join("&", parameterString)))); // refactor
+                request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+            } 
+            else
+            {
+                request.Method = HttpMethod.Get;
+                url += $"?{string.Join("&", parameterString)}";
+            }
+
+            request.RequestUri = new Uri(url);
+            var response = await HttpClient.SendAsync(request);
+            var result = await JsonSerializer.DeserializeAsync<T>(await response.Content.ReadAsStreamAsync()).ConfigureAwait(false);
+
+            if(result == null)
+            {
+                throw new InvalidOperationException("JsonSerializer.DeserializeAsync return null");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Eurotorg-trainee/Service/CurrencyAPI.cs
@@ -0,0 +1,20 @@
+﻿using Eurotorg_trainee.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApiRequest = Core.Helpers.ApiCall;
+
+namespace Eurotorg_trainee.Service
+{
+    public class CurrencyAPI
+    {
+        private readonly ApiRequest apiRequest = new ApiRequest();
+
+        public async ValueTask<IEnumerable<Currency>> GetCurrencyList()
+        {
+            var currencies = await apiRequest
+                .GetAsync<IEnumerable<Currency>>("https://api.nbrb.by/exrates/currencies")
+                .ConfigureAwait(false);
+            return currencies;
+        }
+    }
+}
