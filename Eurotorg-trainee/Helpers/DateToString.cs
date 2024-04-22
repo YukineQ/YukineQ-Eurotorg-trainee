90c9ab183045f7eabe4b078f72e755493e0df8ed
YukineQ <mayberainq@gmail.com>
2024-04-22 17:10:48 +0300

    date formater

---
+++ Eurotorg-trainee/Helpers/DateToString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eurotorg_trainee.Helpers
+{
+    public class DateConveter
+    {
+        public static string Convert(DateTime dateToConver, string format = "yyyy-MM-dd")
+        {
+            return dateToConver.ToString(format);
+        }
+    }
+}
